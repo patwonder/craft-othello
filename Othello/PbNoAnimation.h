d4ece04f639c922bc01349dd79d20abb40bc0fb4
Yifan Wu <patwonder@163.com>
2011-04-19 10:12:23 +0000

    Missing 2 files in last rev - added.

---
+++ Othello/PbNoAnimation.h
@@ -0,0 +1,43 @@
+﻿/*
+*************************************************************************
+    Craft is an othello program with relatively high AI.
+    Copyright (C) 2008-2011  Patrick
+
+    This file is part of Craft.
+
+    Craft is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Craft is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Craft.  If not, see <http://www.gnu.org/licenses/>.
+
+    Craft-Othello on Google Code: <http://code.google.com/p/craft-othello/>
+
+    Patrick's E-mail: patrick880905@sina.com
+    Patrick's Blog: <http://blog.sina.com.cn/patwonder>
+*************************************************************************
+*/
+
+#pragma once
+/*
+********************************************************************************
+								PbNoAnimation.h
+		作者：Patrick
+		概述：setProgressBarValueNoAnimation的声明，用于无动画地设置进度条的值
+			 (Vista/7 only)
+
+********************************************************************************
+*/
+
+namespace Othello {
+
+void setProgressBarValueNoAnimation(System::Windows::Forms::ProgressBar^ pb, int value);
+
+} // namespace Othello
